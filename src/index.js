f693e4d49b867fc6e0640c4a45dc9821eca8b540
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-03-08 22:30:40 -0300

    fix: loader slider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import initDefaultSliders from "./helpers/sliders";
 import "./styles/main.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-	initDefaultSliders();
-
 	document.body.classList.add("visible");
+});
+
+window.addEventListener("load", () => {
+	initDefaultSliders();
 });
