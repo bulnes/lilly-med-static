d2686e4ea8627db20e85f201a53b470e102fdddb
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-03-13 21:37:05 -0300

    feat: update slider rules

--- src/helpers/sliders.js
+++ src/helpers/sliders.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-plusplus */
 import Splide from "@splidejs/splide";
 
 const defaultSplide = {
 	mediaQuery: "min",
+	type: "slide",
 	trimSpace: false,
 	focus: "center",
 	perPage: 3,
@@ -14,14 +16,27 @@ const defaultSplide = {
 	interval: 4000,
 	breakpoints: {
 		768: {
+			type: "loop",
 			trimSpace: true,
-			focus: "center",
+			focus: 0,
 			perPage: 3,
 			autoHeight: true,
 			autoWidth: true,
 			gap: 33,
 			autoplay: false,
 			interval: 4000,
+		},
+	},
+};
+
+const defaultAtLeat3Splide = {
+	...defaultSplide,
+	breakpoints: {
+		768: {
+			...defaultSplide.breakpoints[768],
+			type: "slide",
+			arrows: false,
+			pagination: false,
 		},
 	},
 };
@@ -38,9 +53,10 @@ const bannerSplide = {
 	gap: 23,
 	autoplay: true,
 	interval: 4000,
+	arrows: true,
+	pagination: true,
 	breakpoints: {
 		1200: {
-			type: "loop",
 			trimSpace: false,
 			focus: "center",
 			perPage: 1,
@@ -55,12 +71,29 @@ const bannerSplide = {
 	},
 };
 
+const bannerWith1Splide = {
+	...bannerSplide,
+	type: "slide",
+	arrows: false,
+	pagination: false,
+};
+
+function getConfig(children, isBanner) {
+	if (isBanner) {
+		return children.length < 2 ? { ...bannerWith1Splide } : { ...bannerSplide };
+	}
+	return children.length < 4
+		? { ...defaultAtLeat3Splide }
+		: { ...defaultSplide };
+}
+
 function initDefaultSliders() {
 	const elms = document.getElementsByClassName("splide");
 	for (let i = 0; i < elms.length; i++) {
-		const configs = elms[i].classList.contains("splide--banner")
-			? bannerSplide
-			: defaultSplide;
+		const isBanner = elms[i].classList.contains("splide--banner");
+		const children = [...elms[i].querySelectorAll(".splide__slide")];
+
+		const configs = getConfig(children, isBanner);
 
 		new Splide(elms[i], { ...configs }).mount();
 	}
